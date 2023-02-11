4c76fee7a86cde9311898f76c1edb5a7991f4123
alff0x1f <alff0x1f@gmail.com>
2023-02-12 01:13:40 +0500

    if/else

--- 06_if_else.go
+++ 06_if_else.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+func main() {
+	if 7%2 == 0 {
+		fmt.Println("7 четное")
+	} else {
+		fmt.Println("7 нечетное")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 делится на 4")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "негативное")
+	} else if num < 10 {
+		fmt.Println(num, "из одного числа")
+	} else {
+		fmt.Println(num, "из нескольких цифр")
+	}
+
+}
