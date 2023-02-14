dbfaebdce4e430ee58454619d814cbcf522579ce
alff0x1f <alff0x1f@gmail.com>
2023-02-14 13:05:19 +0500

    methods

---
+++ 20_methods.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type rect struct {
+	width, height int
+}
+
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+func (r *rect) perim() int {
+	return 2*r.width + 2*r.height
+}
+
+func main() {
+	r := rect{width: 10, height: 5}
+	fmt.Println("Area:", r.area())
+	fmt.Println("Perim:", r.perim())
+
+	rPointer := &r
+	fmt.Println("Area:", rPointer.area())
+	fmt.Println("Perim:", rPointer.perim())
+
+	r2 := *rPointer
+	fmt.Println("Area:", r2.area())
+	fmt.Println("Perim:", r2.perim())
+}
